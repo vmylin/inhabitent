e56bf09d1be355f55c63d6dafdde8c2ad7938fe3
Victor Lin <vlinmy06@gmail.com>
2019-11-08 22:45:53 -0800

    update html: code footer.php

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -9,12 +9,29 @@
 
 			</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+		<footer class="site-footer">
+			<div class="footer-container">
+				<div class="contact">
+					<h3>contact info</h3>
+					<p><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></p>
+					<p><a href="tel:778-456-7891">778-456-7891</a></p>
+					<p>
+						<span></span>
+						<span></span>
+						<span></span>
+					</p>
+				</div>
+				<div class="hours">
+					<h3>business hours</h3>
+					<p>monday-friday: 9am to 5pm</p>
+					<p>saturday: 10am to 2pm</p>
+					<p>sunday: closed</p>
+				</div>
+				<img src="<?php echo get_stylesheet_directory_uri();?>/project-04/images/logos/inhabitent-logo-text.svg?>" alt="inhabitent footer logo">
+			</div>
+			<p class="copyright"><span>copyright</span>&copy; 2019 <span>inhabitent</span></p>
+		</footer>		
+	</div><!-- #page -->
 
 		<?php wp_footer(); ?>
 
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -22,14 +22,18 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<div class="logo">
+						<a>
+							<img src="<?php echo get_stylesheet_directory_uri();?>/project-04/images/logos/inhabitent-logo-tent.svg?>" alt="">
+						</a>
+					</div>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
+				<div class="search-form"><?php echo get_search_form();?></div>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
