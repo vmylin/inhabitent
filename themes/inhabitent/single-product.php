2bf0f245bcc594a57aff738a3e1c54d1e09b24a2
Victor Lin <vlinmy06@gmail.com>
2019-11-25 21:27:53 -0800

    add class to avoid conflict on another file

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -11,15 +11,15 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
+		<div class="single-product">
 		<?php while (have_posts()) : the_post(); ?>
-
 			<div class="product">
 				<?php get_template_part('template-parts/content', 'single-product'); ?>
 
 			</div>
-
-		<?php endwhile; // End of the loop. 
+			<?php endwhile; // End of the loop. 
 		?>
+		</div>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
