85e0c6dcd0a23594447b6618f1a91d0361cd4d64
Victor Lin <vlinmy06@gmail.com>
2019-11-25 21:28:22 -0800

    changed h2 to <p>

--- themes/inhabitent/template-parts/content-taxonomy.php
+++ themes/inhabitent/template-parts/content-taxonomy.php
@@ -14,7 +14,7 @@
             <?php the_post_thumbnail('large'); ?>
         <?php endif; ?>
 
-        <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
+        <?php the_title(sprintf('<p class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></p>'); ?>
 
     </header><!-- .entry-header -->
 
