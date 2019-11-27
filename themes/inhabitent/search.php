5f0a32fa2ca67777d234f0d00864eae371847ad5
Victor Lin <vlinmy06@gmail.com>
2019-11-26 20:36:35 -0800

    add .search-page & .side-bar

--- themes/inhabitent/search.php
+++ themes/inhabitent/search.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying search results pages.
  *
@@ -7,32 +8,34 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+<section id="primary" class="content-area">
+	<div class="search-page">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+			<?php if (have_posts()) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+				<header class="page-header">
+					<h1 class="page-title"><?php printf(esc_html('Search Results for: %s'), '<span>' . get_search_query() . '</span>'); ?></h1>
+				</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php /* Start the Loop */ ?>
+				<?php while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'search' ); ?>
+					<?php get_template_part('template-parts/content', 'search'); ?>
 
-			<?php endwhile; ?>
 
-			<?php red_starter_numbered_pagination(); ?>
+				<?php endwhile; ?>
 
-		<?php else : ?>
+				<?php red_starter_numbered_pagination(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php else : ?>
 
-		<?php endif; ?>
+				<?php get_template_part('template-parts/content', 'none'); ?>
 
+			<?php endif; ?>
 		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
+		<div class="side-bar">
+			<?php get_sidebar(); ?></div>
+	</div>
+</section><!-- #primary -->
 <?php get_footer(); ?>
