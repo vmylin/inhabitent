575b22a7a129102407f3ff46d59f4bed8c877346
Victor Lin <vlinmy06@gmail.com>
2019-11-26 20:46:01 -0800

    add read-more button

--- themes/inhabitent/template-parts/content-search.php
+++ themes/inhabitent/template-parts/content-search.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying results in search pages.
  *
@@ -9,16 +10,19 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
+		<?php if ('post' === get_post_type()) : ?>
+			<div class="entry-meta">
+				<?php red_starter_posted_on(); ?> / <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?> / <?php red_starter_posted_by(); ?>
+			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
+		<p><?php the_title(sprintf('<a href="%s"', esc_url(get_permalink())), '>');
+			?>Read More <i class="fas fa-long-arrow-alt-right"></i></a></p>
+
 	</div><!-- .entry-summary -->
 </article><!-- #post-## -->
