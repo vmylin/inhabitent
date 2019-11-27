5a31275c1f6db38f8dc3117a791581bc4372dd01
Victor Lin <vlinmy06@gmail.com>
2019-11-26 22:08:22 -0800

    made click into a function & make callback easier

--- themes/inhabitent/js/style.js
+++ themes/inhabitent/js/style.js
@@ -1,9 +1,18 @@
 (function ($) {
     //on clicking the search button, I want to translate the search bar into view and type
+    function clickSearch() {
 
-    $('.search-submit').on('click', function (e) {
-        e.preventDefault();
-        // 
-        $('.main-navigation').css({ 'transform': 'translateX(0px)', 'transition': '.3s' });
+        $('.search-submit').on('click', function (e) {
+            e.preventDefault();
+            $('.main-navigation').css({ 'transform': 'translateX(0px)', 'transition': '.3s' });
+
+            // $('.search-field').focus();
+
+        });
+    }
+    $('.search-field').focusout(function () {
+        $('.main-navigation').css({ 'transform': 'translateX(175px)', 'transition': '.3s' });
+
     });
+    clickSearch();
 })(jQuery);//end of doc ready
